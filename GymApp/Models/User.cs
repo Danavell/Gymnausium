1524209e6971ad76e095f5f238f756ef972d5189
James <34218837+Danavell@users.noreply.github.com>
2018-10-31 13:55:17 +0100

    User, IUser interface and beginings of DAO

---
+++ GymApp/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class User
+    {
+        public string User_Guid { get; set; }
+        public bool active_account { get; set; }
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string location_data { get; set; }
+        public string description { get; set; }
+        public DateTime last_login { get; set; }
+
+    }
+}
