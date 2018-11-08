3011ee26232020e49665ebcaab9b58f1eaf1ac3a
James <34218837+Danavell@users.noreply.github.com>
2018-11-08 11:29:30 +0100

    InternalInfoUser update

--- GymApp/WCF tests/User Account Tests/user_account_tests.cs
+++ GymApp/WCF tests/User Account Tests/user_account_tests.cs
@@ -19,21 +19,31 @@ namespace Database_Tests
         [TestMethod]
         public void Validation_Test()
         {
-            bool x = Validation_Outcome("email", "password").Result;
-            Assert.IsTrue(x);
+            bool? x = Validation_Outcome("email", "password").Result;
+            if(x != null)
+                Assert.IsTrue((bool)x);
         }
 
         [TestMethod]
         public void Validation_Fail_Test()
         {
-            bool x = Validation_Outcome("false", "false").Result;
-            Assert.IsFalse(x);
+            bool? x = Validation_Outcome("false", "false").Result;
+            if (x != null)
+                Assert.IsFalse((bool)x);
         }
-        
+
+        [TestMethod]
         public void Update_User_Test()
         {
-            bool x = Update_Outcome().Result;
+            bool x = Update_Outcome("67C4E11D-97DC-49C1-85FB-84516519AA7E").Result;
             Assert.IsTrue(x);
+        }
+
+        [TestMethod]
+        public void Update_User_Test_Fail()
+        {
+            bool x = Update_Outcome("67C4E11D-97DC-49C1-85FB-84716519AB7E").Result;
+            Assert.IsFalse(x);
         }
 
         public async Task<bool> Create_Outcome()
@@ -44,17 +54,17 @@ namespace Database_Tests
             return await userDAO.Create(new InternalInfoUser(g, 0, 100, "email", "password", "fname", "lname", 100, "desc"));
         }
 
-        public async Task<bool> Validation_Outcome(string email, string password)
+        public async Task<bool?> Validation_Outcome(string email, string password)
         {
             UserDAO userDAO = new UserDAO();
             return await userDAO.Login_Validation(email, password);
         }
 
-        public async Task<bool> Update_Outcome()
+        public async Task<bool> Update_Outcome(string guid)
         {
             UserDAO userDAO = new UserDAO();
-            Guid g = Guid.NewGuid();
-            return await userDAO.Update(new InternalInfoUser(g, 0, 100, "email", "password", "fname", "lname", 100, "desc"));
+            Guid g = Guid.Parse(guid);
+            return await userDAO.Update(new InternalInfoUser(g, 0, 100, "email", "password", "fname", "lname", 1000, "desc"));
         }
     }
 }
---
+++ GymApp/WCF tests/WCF Tests/WCF_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GymAppService;
+using Model_Layer;
+using Data_Access_Layer;
+using Moq;
+using System.Threading.Tasks;
+
+namespace WCF_tests
+{
+    [TestClass]
+    public class WCF_Tests
+    {
+        [TestMethod]
+        public async Task CreateTest()
+        {
+            MockDAO mock = new MockDAO();
+            UserService userservice = new UserService(mock);
+            InternalInfoUser iiu = new InternalInfoUser(new Guid(), 0, 100, "email@email.com", "password", "fname", "lname", 100, "desc");
+            bool x = await userservice.Create(iiu);
+            Assert.IsTrue(x);
+        }
+
+        [TestMethod]
+        public async Task UpdateTest()
+        {
+            MockDAO mock = new MockDAO();
+            UserService userservice = new UserService(mock);
+            InternalInfoUser iiu = new InternalInfoUser(new Guid(), 0, 100, "email@email.com", "password", "fname", "lname", 100, "desc");
+            bool x = await userservice.Update(iiu);
+            Assert.IsTrue(x);
+        }
+    }
+}
