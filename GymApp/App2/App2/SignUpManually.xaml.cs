51ec1bbbe461827e002e13fa2e78172cc3989e43
jendrekpb <jendrekpb@gmail.com>
2018-11-21 23:17:47 +0100

    Changing the

---
+++ GymApp/App2/App2/SignUpManually.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinClient
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    
+	public partial class SignUpManually : ContentPage
+	{
+      //  (ID, gender, weght, email, psswrd, first_name, last_name, age, descrption, dsbld)
+        Entry password = new Entry  { IsPassword = true, Placeholder = "Password" };
+        Entry username = new Entry {Placeholder = "Username" };
+        Entry username = new Entry { Placeholder = "Username" };
+        Entry username = new Entry { Placeholder = "Username" };
+        Entry username = new Entry { Placeholder = "Username" };
+        Entry username = new Entry { Placeholder = "Username" };
+        Entry username = new Entry { Placeholder = "Username" };
+
+        public SignUpManually ()
+		{
+			InitializeComponent ();
+
+            StackLayout panel = new StackLayout;
+
+            
+		}
+	}
+}
---
+++ GymApp/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Model_Layer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GymAppService;
+
+namespace Controllers
+{
+    class UserController
+    {
+        UserService service = new UserService();
+        public async System.Threading.Tasks.Task CreateUserAsync(Guid user_guid, int gender, int weight, string email, string password, string fname, string lname, int age, string desc, bool disabled)
+        {
+            InternalInfoUser user = new InternalInfoUser(user_guid, gender, weight, email, password, fname, lname, age, desc, disabled);
+            await service.Create(user);  
+        
+        }
+
+        // To be implemented
+        public void DeleteUser()
+        {
+
+        }
+        public void UpdateUser()
+        {
+
+        }
+        public void GetUser()
+        {
+
+        }
+    }
+}
