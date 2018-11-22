ae3750a32d64fa9bb0475fe3ca60b28a80527daa
jendrekpb <jendrekpb@gmail.com>
2018-11-22 21:53:02 +0100

    controller adapted to changes with USer

--- GymApp/Controllers/UserController.cs
+++ GymApp/Controllers/UserController.cs
@@ -11,9 +11,10 @@ namespace Controllers
         UserService service = new UserService();
         public async System.Threading.Tasks.Task CreateUserAsync(Guid user_guid, int gender, int weight, string email, string password, string fname, string lname, int age, string desc, bool disabled)
         {
-            InternalInfoUser user = new InternalInfoUser(user_guid, gender, weight, email, password, fname, lname, age, desc, disabled);
+            Guid guid = new Guid();
+            InternalInfoUser user = new InternalInfoUser(guid, gender, weight, email, password, fname, lname, age, desc);
             await service.Create(user);  
-        
+            
         }
 
         // To be implemented
--- GymApp/Model Layer/ExternalInfoUser.cs
+++ GymApp/Model Layer/ExternalInfoUser.cs
@@ -9,7 +9,7 @@ namespace Model_Layer
     public class ExternalInfoUser : User
     {
         [DataMember] public float Distance { get; set; }
-        public ExternalInfoUser() { }
+        public ExternalInfoUser(int user_guid) { }
         public ExternalInfoUser(Guid user_guid, string fname, int age, int gender, int weight, string desc, float distance) : base(user_guid, gender, weight, fname, age, desc)
         {
             Distance = distance;
