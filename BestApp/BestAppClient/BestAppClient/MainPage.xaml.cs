58d955c5802f700fc40c4ca006cdc6701155c4fa
akosskutovics <skutovics.akos@gmail.com>
2018-11-23 00:07:01 +0100

    goelocation update (not finished)

--- BestApp/BestAppClient/BestAppClient/MainPage.xaml.cs
+++ BestApp/BestAppClient/BestAppClient/MainPage.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Geolocator;
+using System.Diagnostics;
+using Plugin.Geolocator.Abstractions;
 
 namespace BestAppClient
 {
@@ -13,5 +16,30 @@ namespace BestAppClient
         {
             InitializeComponent();
         }
+        private async void ButtonClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("dick");
+            if (IsLocationAvailable())
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+                
+                var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(100),null,true); //this shit gives an error
+
+                //    Debug.WriteLine("Position Status: {0}", position.Timestamp);
+                //    Debug.WriteLine("Position Latitude: {0}", position.Latitude);
+                //    Debug.WriteLine("Position Longitude: {0}", position.Longitude);
+               
+            }
+
+        }
+        public bool IsLocationAvailable()
+        {
+            if (!CrossGeolocator.IsSupported)
+                return false;
+
+            return CrossGeolocator.Current.IsGeolocationAvailable;
+        }
+
     }
 }
