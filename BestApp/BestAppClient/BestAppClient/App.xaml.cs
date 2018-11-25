0060d3f935c0f5b5c7e3de913ca039f1db670a91
Vít Vašák <vitvasak@outlook.com>
2018-11-25 18:52:18 +0100

    Comments

--- BestApp/BestAppClient/BestAppClient/App.xaml.cs
+++ BestApp/BestAppClient/BestAppClient/App.xaml.cs
@@ -37,15 +37,20 @@ namespace BestAppClient
             MainPage = page;
         }
         /// <summary>
-        /// Gets login credentials stored from device
+        /// Gets login credentials stored from device. Key: username, Value: password
         /// </summary>
-        /// <returns>Key: username, Value: password</returns>
+        /// <returns></returns>
         public static async Task<KeyValuePair<string, string>> GetLoginCredentialsAsync()
         {
             var username = await SecureStorage.GetAsync("username");
             var password = await SecureStorage.GetAsync("password");
             return new KeyValuePair<string, string>(username, password);
         }
+        /// <summary>
+        /// Store login credential for automatic login. Data is encrypted.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
         public static async void StoreCredentialsToDeviceAsync(string username, string password)
         {
             await SecureStorage.SetAsync("username", username);
