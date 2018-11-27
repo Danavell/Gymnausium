b70f6dcd97336a858a2e4d5a2b63981903f9881c
jendrekpb <jendrekpb@gmail.com>
2018-11-27 15:36:44 +0100

    Deleted model layer working on SIngup

--- GymApp/BestAppClient/BestAppClient/Views/SignUpManually.xaml.cs
+++ GymApp/BestAppClient/BestAppClient/Views/SignUpManually.xaml.cs
@@ -9,10 +9,39 @@ using Xamarin.Forms.Xaml;
 
 namespace BestAppClient.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SignUpManually : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
+
+    public partial class SignUpManually : ContentPage
 	{
-		public SignUpManually ()
+        private int gender;
+        Entry genderEntry;
+
+        private int weight;
+        Entry weightEntry;
+
+        private string email;
+        Entry emailEntry;
+
+        private string password;
+        Entry passwordEntry;
+
+        private string fname;
+        Entry fnameEntry;
+
+        private string lname;
+        Entry lnameEntry;
+
+        private int age;
+        Entry ageEntry;
+
+        private string desc;
+        Entry descEntry;
+
+        private bool disabled;
+       
+
+        public SignUpManually ()
 		{
 			InitializeComponent();
 		}
